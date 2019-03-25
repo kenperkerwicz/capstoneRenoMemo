c41883562da49556b78810556789aa19baeea619
apple <kenperkerwicz>
2019-03-25 17:56:08 -0500

    added background image on home page

--- src/components/home/HomeList.js
+++ src/components/home/HomeList.js
@@ -11,15 +11,17 @@ export default class HomeList extends Component {
     let user = Number(sessionStorage.getItem("credentials"))
       return (
           <React.Fragment>
-<div className="entireImageDiv">
-            {/* <Row> */}
-            {/* <img className="homeImage"src="https://images.unsplash.com/photo-1527722681152-c019fcfe8858?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80" alt="home"></img>
-            <img className="homeImage"src="https://images.unsplash.com/photo-1414353220870-38cc1d0fbf76?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=967&q=80" alt="home"></img> */}
 
+<div className="entireImageDiv">
+            <Row>
+              {/* <Row id="firstRow">
+           <img className="homeImage" id="firstImage"src="https://images.unsplash.com/photo-1527722681152-c019fcfe8858?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80" alt="home"></img>
+            <img className="homeImage"src="https://images.unsplash.com/photo-1414353220870-38cc1d0fbf76?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=967&q=80" alt="home"></img>
+            </Row> */}
             <img className="homeImage"src="https://images.unsplash.com/photo-1501635238895-63f29cfc06b3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1051&q=80" alt="home"></img>
+          <img className="homeImage"src="https://images.unsplash.com/photo-1551354907-80361e454f5a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80" alt="home"></img>
+           </Row>
             </div>
-            {/* <img className="homeImage"src="https://images.unsplash.com/photo-1551354907-80361e454f5a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80" alt="home"></img> */}
-            {/* </Row> */}
 
             <h1 className="homesTitle">Homes.</h1>
               <div
@@ -36,7 +38,7 @@ export default class HomeList extends Component {
 
               </div>
 
-<Row className="homeRow">
+<Row className="homeCardsBackground">
 
         {
           this.props.homes.filter(mv => mv.userId === user)
